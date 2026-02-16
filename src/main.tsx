e84e78dd25af357d3848cabfaad57a7aac82f003
石橋篤弥 <ishibashiatsuya@ishibashiatsushiwatarunoMacBook-Pro.local>
2026-02-16 09:34:10 +0900

    fix: set router basename for github pages

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    {/* GitHub Pages の /repo-name/ 配下でもルーティングが動くようにする */}
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
